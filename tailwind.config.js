666f0a06aaaa294391d01fb1fa449d7bbe8ef0e0
Nirav <koradiyahari@gmail.com>
2022-11-12 19:41:50 +0530

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./*.html"
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
